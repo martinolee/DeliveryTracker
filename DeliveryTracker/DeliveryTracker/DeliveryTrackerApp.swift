9f5c7740a7919f0a233d8e0716a5c5b14f75fe78
martinolee <soohan.m.lee@gmail.com>
2024-12-24 17:45:32 +0900

    Add default project files

---
+++ DeliveryTracker/DeliveryTracker/DeliveryTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliveryTrackerApp.swift
+//  DeliveryTracker
+//
+//  Created by Soohan Lee on 12/24/24.
+//
+
+import SwiftUI
+
+@main
+struct DeliveryTrackerApp: App {
+  var body: some Scene {
+    WindowGroup {
+      EmptyView()
+    }
+  }
+}
